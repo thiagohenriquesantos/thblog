51b1438e1af6b684dcc3adf501b373e16635c6ab
Thiago Santos <thiagosantosdev@gmail.com>
2022-12-20 11:39:26 -0300

    Author implementado

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Image from 'next/image';
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>
-      <h1>
-        Author
-      </h1>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolut left-0 right-0 -top-14">
+        <Image
+          alt={author.name}
+          unoptimized
+          height={100}
+          width={100}
+          className='align-middle rounded-full inline'
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+      <p className='text-white text-lg'>{author.bio}</p>
     </div>
   )
 }
